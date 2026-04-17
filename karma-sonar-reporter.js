0a6b0a7657bb0e496cbd5f00c72d86fd16be0341
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-04-17 21:29:18 +0530

    changes in config.yml and karma.conf.js

--- karma-sonar-reporter.js
+++ karma-sonar-reporter.js
@@ -9,35 +9,43 @@ function SonarReporter(baseReporterDecorator, config, logger) {
   let testCases = [];
 
   this.onSpecComplete = function (browser, result) {
-    testCases.push({
-      name: result.fullName,
-      duration: result.time || 0,
-      status: result.success ? 'passed' : (result.skipped ? 'skipped' : 'failed')
-    });
-  };
+  testCases.push({
+    name: result.fullName,
+    duration: result.time || 0,
+    status: result.success ? 'passed' : (result.skipped ? 'skipped' : 'failed'),
+    file: result.file || 'test/client/unknown.js'   // 👈 FIX
+  });
+};
 
   this.onRunComplete = function () {
-    let xml = `<testExecutions version="1">\n`;
-    xml += `  <file path="karma-tests">\n`;
+  let xml = `<testExecutions version="1">\n`;
 
-    testCases.forEach(tc => {
+  const grouped = {};
+
+  testCases.forEach(tc => {
+    if (!grouped[tc.file]) grouped[tc.file] = [];
+    grouped[tc.file].push(tc);
+  });
+
+  Object.keys(grouped).forEach(file => {
+    xml += `  <file path="${file}">\n`;
+
+    grouped[file].forEach(tc => {
       xml += `    <testCase name="${tc.name}" duration="${tc.duration}">`;
 
-      if (tc.status === 'failed') {
-        xml += `<failure/>`;
-      } else if (tc.status === 'skipped') {
-        xml += `<skipped/>`;
-      }
+      if (tc.status === 'failed') xml += `<failure/>`;
+      else if (tc.status === 'skipped') xml += `<skipped/>`;
 
       xml += `</testCase>\n`;
     });
 
     xml += `  </file>\n`;
-    xml += `</testExecutions>`;
+  });
 
-    fs.writeFileSync(outputFile, xml);
-    log.info(`Sonar test report generated at ${outputFile}`);
-  };
+  xml += `</testExecutions>`;
+
+  fs.writeFileSync(outputFile, xml);
+};
 }
 
 SonarReporter.$inject = ['baseReporterDecorator', 'config', 'logger'];
